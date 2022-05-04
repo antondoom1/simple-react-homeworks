f44860d200ad0ba9b17474e93313e19a412ff0c7
AntonYakavenka <doomanton@gmail.com>
2022-05-04 13:23:44 +0300

    Updates

---
+++ precache-manifest.b8854f383b2cbe2da765e6fc79592bfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9689a2565cf12604e387570c9ed87472",
+    "url": "/simple-react-homeworks/index.html"
+  },
+  {
+    "revision": "4d2b565865101a5bb1ff",
+    "url": "/simple-react-homeworks/static/css/main.5566122a.chunk.css"
+  },
+  {
+    "revision": "5fe4efd31a64640404d5",
+    "url": "/simple-react-homeworks/static/js/2.3ed90c5f.chunk.js"
+  },
+  {
+    "revision": "248fe9facfff898840212404b4cb88d9",
+    "url": "/simple-react-homeworks/static/js/2.3ed90c5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d2b565865101a5bb1ff",
+    "url": "/simple-react-homeworks/static/js/main.b3bb94d3.chunk.js"
+  },
+  {
+    "revision": "35a9557543d5cb14c0c5",
+    "url": "/simple-react-homeworks/static/js/runtime-main.5a451111.js"
+  }
+]);
