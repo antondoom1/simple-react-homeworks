e313d3cb7af848bca220f4affb6c2d577173d787
AntonYakavenka <doomanton@gmail.com>
2022-05-14 18:48:03 +0300

    HW7

--- src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import HW7 from '../../h7/HW7'
 
 function Junior() {
   return (
     <div>
-      Junior
+
+      <HW7/>
+
     </div>
+
   )
 }
 
--- src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
+++ src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
@@ -1,40 +1,46 @@
 import React, {ChangeEvent, InputHTMLAttributes, DetailedHTMLProps} from 'react'
+import s from './SuperRadio.module.css'
 
 type DefaultRadioPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 type SuperRadioPropsType = DefaultRadioPropsType & {
-    options?: any[]
-    onChangeOption?: (option: any) => void
+  options?: any[]
+  onChangeOption?: (option: any) => void
 }
 
 const SuperRadio: React.FC<SuperRadioPropsType> = (
-    {
-        type, name,
-        options, value,
-        onChange, onChangeOption,
-        ...restProps
-    }
+  {
+    type, name,
+    options, value,
+    onChange, onChangeOption,
+    ...restProps
+  }
 ) => {
-    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        // onChange, onChangeOption
-    }
+  const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange && onChange(e)
+    onChangeOption && onChangeOption(e.currentTarget.value)
+  }
 
+  const mappedOptions: any[] = options ? options.map((o, i) => (
+    <label key={name + '-' + i}>
+      <input
+        type={'radio'}
+        name={o}
+        checked={value === o}
+        value={o}
+        onChange={onChangeCallback}
+      />
+      <span>{o}</span>
+    </label>
+  )) : []
 
-    const mappedOptions: any[] = options ? options.map((o, i) => ( // map options with key
-        <label key={name + '-' + i}>
-            <input
-                type={'radio'}
-                // name, checked, value, onChange
-            />
-            {o}
-        </label>
-    )) : []
-
-    return (
-        <>
-            {mappedOptions}
-        </>
-    )
+  return (
+    <>
+      <form className={s.radio}>
+        {mappedOptions}
+      </form>
+    </>
+  )
 }
 
 export default SuperRadio
