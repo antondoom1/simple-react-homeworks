c37474d4045d58264a3e9653693f3a0c5a0dd199
AntonYakavenka <doomanton@gmail.com>
2022-06-11 16:57:47 +0300

    HW11

--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,24 +1,27 @@
-import React from 'react'
+import React, {ChangeEvent} from 'react'
+import {Slider} from '@material-ui/core'
 
 type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
-    // min, max, step, disable, ...
+  onChangeRange?: (value: number[]) => void
+  value?: number[]
+  step?: number
 }
 
-const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
-    {
-        onChangeRange, value,
-        // min, max, step, disable, ...
-    }
-) => {
-    // сделать самому, можно подключать библиотеки
+const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = ({onChangeRange, value, step}) => {
 
-    return (
-        <>
-            DoubleRange
-        </>
-    )
+  const onChangeCallBack = (e: ChangeEvent<{}>, newValue: number | number[]) => {
+    onChangeRange && onChangeRange(newValue as number[])
+  }
+
+  return (
+    <Slider
+      value={value}
+      onChange={onChangeCallBack}
+      valueLabelDisplay="auto"
+      step={step}
+      color="primary"
+    />
+  )
 }
 
 export default SuperDoubleRange
--- src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
@@ -1,13 +1,27 @@
 import React from 'react'
+import img from './img/404.svg'
+import s from './Error404.module.css'
+import {Button} from '@material-ui/core'
+import {PATH} from '../Pages'
 
 function Error404() {
-    return (
-        <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
-        </div>
-    )
+  return (
+    <div className={s.errorPageWrapper}>
+      <img src={img} alt="404"/>
+      <div className={s.messageBlock}>
+        <h2>Oh no! Where did you go?</h2>
+        <p>We can’t seem to find the page you were looking for.</p>
+        <Button
+          href={PATH.PRE_JUNIOR}
+          color={'primary'}
+          variant={'contained'}
+          className={s.button}
+        >
+          Go back to safety
+        </Button>
+      </div>
+    </div>
+  )
 }
 
 export default Error404
